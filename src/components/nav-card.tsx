0ffd62778b2b716a8732f5255a1f2d28b7259acb
羡鱼 <x9090999@163.com>
2026-06-17 16:06:10 +0800

    Update nav-card.tsx

--- src/components/nav-card.tsx
+++ src/components/nav-card.tsx
@@ -141,12 +141,12 @@ export default function NavCard() {
 					<Link className='flex items-center gap-3' href='/'>
 						<Image src='/images/avatar.png' alt='avatar' width={40} height={40} style={{ boxShadow: ' 0 12px 20px -5px #E2D9CE' }} className='rounded-full' />
 						{form === 'full' && <span className='font-averia mt-1 text-2xl leading-none font-medium'>{siteContent.meta.title}</span>}
-						{form === 'full' && <span className='text-brand mt-2 text-xs font-medium'>(开发中)</span>}
+						//{form === 'full' && <span className='text-brand mt-2 text-xs font-medium'>(学习中)</span>}
 					</Link>
 
 					{(form === 'full' || form === 'icons') && (
 						<>
-							{form !== 'icons' && <div className='text-secondary mt-6 text-sm uppercase'>General</div>}
+							{form !== 'icons' && <div className='text-secondary mt-6 text-sm uppercase'>导航</div>}
 
 							<div className={cn('relative mt-2 space-y-2', form === 'icons' && 'mt-0 flex items-center gap-6 space-y-0')}>
 								<motion.div
