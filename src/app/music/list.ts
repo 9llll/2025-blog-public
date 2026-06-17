6450484fb0e980fde8d1a6830a3e3e5d13bfb2c4
羡鱼 <x9090999@163.com>
2026-06-17 17:07:32 +0800

    Update list.ts

--- src/app/music/list.ts
+++ src/app/music/list.ts
@@ -1,6 +1,6 @@
 export const list = [
 	{
-		name: 'G.E.M.邓紫棋 - 唯一',
+		name: '唯一',
 		iframe: `<iframe src="//player.bilibili.com/player.html?isOutside=true&aid=874221632&bvid=BV1ZN4y1f7p6&cid=1285573415&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>`
 	}
 ]
